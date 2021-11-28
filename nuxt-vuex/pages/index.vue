2689b2735a654011224fca784593841b73445647
向山　翔 <hms0175km@gmail.com>
2021-11-28 18:23:13 +0900

    [Finidh]  ストア ＝ クラシックモードからモジュールモードへ記述を変更

--- nuxt-vuex/pages/index.vue
+++ nuxt-vuex/pages/index.vue
@@ -1,11 +1,14 @@
 <template>
   <section class="container">
     <div>
-    <p>{{ $store.state.message }}</p>
+      <!-- クラシックモード↓(モジュールモードを使うためにindex.jsを削除した) -->
+    <!-- <p>{{ $store.state.message }}</p> -->
     <!-- <button v-on:click="$store.commit('updateMessage')">Update</button> -->
     <!-- <button v-on:click="$store.commit('updateMessage', 'Commit with payload')">Update</button> -->
     <!-- <button v-on:click="$store.dispatch('updateMessageAction', 'Commit with payload')">dispatch</button> -->
-    <button v-on:click="$store.dispatch('updateMessageAction', 'Dispatch with payload')">dispatch</button>
+      <!-- モジュールモード↓ -->
+    <p>{{ $store.state.hello.message }}</p>
+    <button v-on:click="$store.dispatch('hello/updateMessageAction', 'Dispatch with payload')">dispatch</button>
     </div>
   </section>
 </template>
